18c4de5f0fe05c6e08b7dba7d6beda88205b5e9b
KimKiHyun0206 <rlarlgus0206@naver.com>
2024-02-17 16:58:06 +0900

    update : 2.17

--- Ch08-비동기-메시지-전송하기/tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
+++ Ch08-비동기-메시지-전송하기/tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
@@ -18,7 +18,18 @@ public class RabbitOrderReceiver implements OrderReceiver {
   }
   
   public Order receiveOrder() {
+    /*
+    //만약 사용 환경에 따라서 지연을 목표로 할 경우에는 이런 코드를 사용한다
+    Message message = rabbit.recive("tacocloud.order.queue",30000);
+    return message != null ? (Order) converter.fromMessage(message) : null;
+     */
     return (Order) rabbit.receiveAndConvert("tacocloud.order.queue");
+
+    /*
+    //위 방법보다 더 간단한 방법은 아래와 같다
+    //이 방법은 PatameterizedTypeReference를 직접 인자로 전달하여 Order 객체를 수신하게 하는 것이다
+    return rabbit.receiveAndConvert("tacocloud.order.queue", new PatameterizedTypeReference<Order>() {});
+     */
   }
   
 }
